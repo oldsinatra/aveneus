e25bf32d3f7a1b836648a50ee824e448902ee686
jkinathan <jkinobe@gmail.com>
2022-04-15 10:48:48 +0300

    fix: Regenerates migrations specific to properties app

--- aveneus_project/settings.py
+++ aveneus_project/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django.contrib.sites',
     'properties',
-    'api',
+    # 'api',
     'corsheaders',
     'rest_framework',
     'rest_framework.authtoken',
--- properties/models.py
+++ properties/models.py
@@ -25,7 +25,7 @@ class Property(models.Model):
     title = models.CharField(max_length=250)
     type = models.CharField(max_length=250)
     # landlord = models.CharField(max_length=100)
-    landlord = models.ForeignKey(Landlord, on_delete=models.CASCADE)
+    landlord = models.ForeignKey(Landlord, on_delete=models.CASCADE, null=True)
     tenant = models.ForeignKey(Tenant, on_delete=models.CASCADE, blank=True, null=True)
     total_units = models.CharField(max_length=13)
     occupied_units = models.CharField(max_length=13)
@@ -41,9 +41,9 @@ class Property(models.Model):
 
 class PropertyTenant(models.Model):
     name = models.CharField(max_length=250)
-    propertyId =  models.ForeignKey(Property, on_delete=models.CASCADE)
-    landlordId =  models.ForeignKey(Landlord, on_delete=models.CASCADE)
-    tenantId =  models.ForeignKey(Tenant, on_delete=models.CASCADE, blank=True, null=True)
+    property =  models.ForeignKey(Property, on_delete=models.CASCADE, null=True)
+    landlord =  models.ForeignKey(Landlord, on_delete=models.CASCADE, null=True)
+    tenant =  models.ForeignKey(Tenant, on_delete=models.CASCADE, blank=True, null=True)
     period  = models.CharField(max_length=250, blank=True, null=True)
     price  = models.CharField(max_length=250, blank=True, null=True)
     amountDue  = models.CharField(max_length=250, blank=True, null=True)
